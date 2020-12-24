c089a296dc9344d8c1182e4967c0a03104cff0a4
Ernesto Avila Domenech <eadomenech@gmail.com>
2020-12-24 15:50:58 -0500

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/eadomenech/pyJaya',
-    version='0.2.1',
+    version='0.3.0',
     zip_safe=False,
 )
