9a39faa1996c9727e0e1b610bf462348bd863a95
Ernesto Avila Domenech <eadomenech@gmail.com>
2020-12-27 14:17:29 -0500

    update usage

--- examples/Himmelblau.py
+++ examples/Himmelblau.py
@@ -12,16 +12,40 @@ from pyJaya.variables import VariableFloat
 
 
 def himmelblau(solution):
+    """Himmelblau function
+
+    Args:
+        solution (Solution): Candidate solution
+
+    Returns:
+        float: Himmelblau function value
+    """
     return (
         solution[0] ** 2 + solution[1] - 11) ** 2 +\
             (solution[0] + solution[1] ** 2 - 7) ** 2
 
 
 def himmelblauConstraintOne(solution):
+    """First restriction
+
+    Args:
+        solution (Solution): Candidate solution
+
+    Returns:
+        bool: True if it meets the constraint, False otherwise
+    """
     return (26 - (solution[0] - 5) ** 2 - solution[1] ** 2) >= 0
 
 
 def himmelblauConstraintTwo(solution):
+    """Second restriction
+
+    Args:
+        solution (Solution): Candidate solution
+
+    Returns:
+        bool: True if it meets the constraint, False otherwise
+    """
     return (20 - 4 * solution[0] - solution[1]) >= 0
 
 
